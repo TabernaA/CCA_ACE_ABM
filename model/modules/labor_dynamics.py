8a16194a522c51b89524d4087f193a536d37f0d8
TabernaA <53480277+TabernaA@users.noreply.github.com>
2020-12-02 15:53:51 +0000

    Add files via upload

---
+++ model/modules/labor_dynamics.py
@@ -0,0 +1,192 @@
+'''
+model/modules/labor_dynamics.py
+
+Functions for Labor Dynamics
+labor_search()   : for households
+labor_demand()   : for firms
+hire_and_fire()  : for firms
+
+'''
+import random
+import math
+import numpy as np
+
+
+''' Labor Search:
+Each unemployed household searches through the available suitable_employers
+in both sectors and picks the employer offering the highest wage.
+
+PARAMETERS
+employer : reference to my employer
+model    : reference to the model
+pos      : reference to my position (self.pos)
+'''
+def labor_search(unique_id, employer_ID, model,  region):
+    if employer_ID == None:
+    
+        suitable_employers_IDs = []
+        # get the list of all the firms living in the region 
+        region_employers = model.datacollector.model_vars["Firms_regions"][int(model.schedule.time)][region]
+
+        # find rhe ones with open vacancies in my region
+        for i in range(len(region_employers)):
+            if ( region_employers[i][0].open_vacancies == True):
+                suitable_employers_IDs.append(region_employers[i][1])
+        #print("SUITABLE EMPLOYERS IDS", suitable_employers_IDs)
+
+        if len(suitable_employers_IDs) > 0:
+            # due to bounded rationality, choose from a subset of all suitable employers, in this case is 1/4 of the total
+            possible_employers_IDs = random.sample(suitable_employers_IDs, math.ceil(len(suitable_employers_IDs)/4))
+
+            # choose the employer who offers the highest wage
+            if len(possible_employers_IDs) > 0:
+                possible_employers_wages = []
+                for id in possible_employers_IDs:
+                    
+                    possible_employer = model.schedule.agents[id]
+                    possible_employers_wages.append(possible_employer.wage)
+
+                best_i = possible_employers_wages.index(max(possible_employers_wages))
+                employer = model.schedule.agents[possible_employers_IDs[best_i]]
+                employer.employees_IDs.append(unique_id)
+                employer_ID = possible_employers_IDs[best_i]
+
+                if employer.desired_employees == len(employer.employees_IDs):
+                      employer.open_vacancies = False
+
+
+
+        return employer_ID
+
+####----------Same procedure as labor search, but only for capital firms, used if we want to separate the labor market matching between the sectors (i.e. capital good go first since they pay more)
+
+def labor_search_cap(unique_id, employer_ID, model, region):
+    if employer_ID == None:
+        suitable_employers_IDs = []
+        region_employers = model.datacollector.model_vars["Firms_regions"][int(model.schedule.time)][region]
+        region_employers = region_employers[0]
+        # find all firms with open vacancies in my region
+        for i in range(len(region_employers)):
+            if ( region_employers[i][0].open_vacancies == True):
+                suitable_employers_IDs.append(region_employers[i][1])
+        #print("SUITABLE EMPLOYERS IDS", suitable_employers_IDs)
+
+        if len(suitable_employers_IDs) > 0:
+            # due to bounded rationality, choose from a subset of all suitable employers
+            possible_employers_IDs = random.sample(suitable_employers_IDs, math.ceil(len(suitable_employers_IDs)/4))
+
+            # choose the employer who offers the highest wage
+            if len(possible_employers_IDs) > 0:
+                possible_employers_wages = []
+                for id in possible_employers_IDs:
+                    possible_employer = model.schedule.agents[id]
+                    possible_employers_wages.append(possible_employer.wage)
+
+                best_i = possible_employers_wages.index(max(possible_employers_wages))
+                employer = model.schedule.agents[possible_employers_IDs[best_i]]
+                employer.employees_IDs.append(unique_id)
+                employer_ID = possible_employers_IDs[best_i]
+
+                if employer.desired_employees == len(employer.employees_IDs):
+                      employer.open_vacancies = False
+
+    return employer_ID
+
+
+
+####----------Same procedure as labor search, but only for consumption firms,
+
+def labor_search_cons(unique_id, employer_ID, model, region):
+    if employer_ID == None:
+        suitable_employers_IDs = []
+        region_employers = model.datacollector.model_vars["Firms_regions"][int(model.schedule.time)][region]
+        region_employers = region_employers[1]
+        # find all firms with open vacancies in my region
+        for i in range(len(region_employers)):
+            if ( region_employers[i][0].open_vacancies == True):
+                suitable_employers_IDs.append(region_employers[i][1])
+        #print("SUITABLE EMPLOYERS IDS", suitable_employers_IDs)
+
+        if len(suitable_employers_IDs) > 0:
+            # due to bounded rationality, choose from a subset of all suitable employers
+            possible_employers_IDs = random.sample(suitable_employers_IDs, math.ceil(len(suitable_employers_IDs)/10))
+
+            # choose the employer who offers the highest wage
+            if len(possible_employers_IDs) > 0:
+                possible_employers_wages = []
+                for id in possible_employers_IDs:
+                    possible_employer = model.schedule.agents[id]
+                    possible_employers_wages.append(possible_employer.wage)
+
+                best_i = possible_employers_wages.index(max(possible_employers_wages))
+                employer = model.schedule.agents[possible_employers_IDs[best_i]]
+                employer.employees_IDs.append(unique_id)
+                #print("gained employee")
+                #print("Employees list", employer.employees_IDs)
+                employer_ID = possible_employers_IDs[best_i]
+
+                if employer.desired_employees == len(employer.employees_IDs):
+                      employer.open_vacancies = False
+
+    return employer_ID
+
+'''
+Firms determination of labor demand
+'''
+def labor_demand(capital_vintage, feasible_production):
+    ## Find the most productive machines for the quantity I want to produce   ##
+    ## Fewest machines needed to satisfy feasible_production ##
+    Q = 0               # cumulative quantity
+    machines_used = []
+
+    # go over the machine vintage backwards, so starting with the most productive machines
+    num_machines = len(capital_vintage)
+    for i in range(1, num_machines+1):
+        if (Q <= feasible_production):
+            machines_used.append(capital_vintage[num_machines - i])
+            Q += np.prod(capital_vintage[num_machines - i].amount)
+   
+
+
+    ## weighted average productivity of the machines I want to use ##
+    average_productivity =  round((sum( [v.amount * v.productivity for v in machines_used] ) + 1) / (sum([v.amount for v in machines_used]) +1), 5)
+    #print("average productivity is ", average_productivity, "my feasible production is ", feasible_production)
+    ##for i in machines_used:
+       # print("amount", i.amount, "productivity", i.productivity)
+    # How much labor is needed to satisfy feasible production, given my average productivity
+    round(feasible_production , 5)
+    labor_demand = max( 0, round(feasible_production / average_productivity , 2))
+    
+
+    return labor_demand, average_productivity
+
+
+
+
+'''
+hire and fire workers
+'''
+def hire_and_fire(labor_demand, employees_IDs, open_vacancies, model, id):
+    # Number of desired employees 
+    desired_employees =  round(labor_demand)
+    #print("I desire", desired_employees, "I have ", len(employeeDesirs_IDs))
+    #Depending on how many employees has vs.how many it wants, the firms post new vancancies or fire
+    if desired_employees == len(employees_IDs):
+        open_vacancies = False
+
+    elif desired_employees > len(employees_IDs):
+        open_vacancies = True
+            
+    elif desired_employees < len(employees_IDs):
+        open_vacancies = False
+        firing_employees = abs( desired_employees - len(employees_IDs))
+        #print("firing employees", firing_employees)
+        for i in range( int(firing_employees)):
+            j = employees_IDs[0]
+            employee = model.schedule.agents[employees_IDs[0]]
+            employee.employer_ID = None
+            del employees_IDs[0]
+        
+
+
+    return desired_employees, employees_IDs, open_vacancies
---
+++ model/modules/migration.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 12:31:50 2020
+@author: TabernaA
+modules/migration.py
+"""
+
+import math 
+import random
+import bisect
+
+
+
+'''
+Calculation of households probability to migrate
+'''
+def households_migration_probability(region, model, my_income, w_1 = 0.5, w_2 = 0.5, mc = 0.05): 
+    ##--retrieve relevant parameters --#
+    unemployment = model.datacollector.model_vars['Unemployment_Regional'][int(model.schedule.time)]
+    average_wage = model.datacollector.model_vars['Average_Salary'][int(model.schedule.time)]
+    current_average_price = model.datacollector.model_vars["Cosumption_price_average"][int(model.schedule.time)]
+    real_income =round( my_income / (current_average_price[region] + 0.0001), 5)
+    real_income_exp = round(average_wage[1-region]/( current_average_price[ 1 - region] + 0.0001),5)
+                               
+    #rint("unemployemt", )
+    ##---calculation of probability --##
+    prob_migration = 1-math.exp(w_1* (  real_income - real_income_exp ) / real_income +
+                                w_2*(unemployment[1-region] + 0.01 - unemployment[region]) / (unemployment[1-region] + 0.001))
+    #prob_migration = prob_migration * (1 - mc)
+    #print("H region ", region, "real income" , real_income, " real_income exp", real_income_exp, "unemployment rate", unemployment[region],
+     #     "Unemployement ex", unemployment[1 - region], " probability", prob_migration)
+    
+    return prob_migration if prob_migration > 0 else 0
+
+
+
+
+'''
+households migration procedure
+'''
+def household_migrate(mp, model, region, unique_id):
+    ##-- if the probability is higher than random number between 0,1, leave the job and move out --##
+	agent = model.schedule._agents[unique_id]
+	if random.uniform(0,1) < mp:
+		# leave the job in the region and become unemployed
+		if agent.employer_ID != None:
+			employer = model.schedule._agents[agent.employer_ID]
+			if unique_id in employer.employees_IDs:
+				employer.employees_IDs.remove(unique_id)
+			agent.employer_ID = None
+        
+        # update the lists of ids for each region, keep them ordered
+		if region == 0:
+			model.ids_region0.remove(unique_id)
+			bisect.insort(model.ids_region1, unique_id)
+		elif region == 1:
+			model.ids_region1.remove(unique_id)
+			bisect.insort(model.ids_region0, unique_id)
+
+		# move the agent in the grid
+		#model.grid.remove_agent(agent)
+		model.grid.move_agent(agent, (0, 1-region))
+
+		
+		#print("Household", unique_id,"migrated to region", 1-region)
+		return 1-region
+	return region
+
+
+
+
+''' 
+	Probability of migration for firms
+	PARAMETERS
+	D       : demand of firm in both regions [0,1]
+	r       : firm's region
+   average_profits_other_reg: prfits made on average by the same sector in the other region
+	my_wage : wage offered by the firm
+'''
+def firms_migration_probability(D, r, my_wage, profits, model, w_1 = 0.4, w_2 = 0.3, w_3 = 0.4, mc = 0.05):
+    
+   ##---collect relevant variables for my time step ---##
+    time = int(model.schedule.time)
+    average_foreign_wage = model.datacollector.model_vars['Average_Salary'][time][1-r]
+    regional_unemployment_subsidy = model.datacollector.model_vars["Regional_unemployment_subsidy"][time][r]
+    minimum_wage = model.datacollector.model_vars["Minimum_wage"][time ]
+    if average_foreign_wage <= minimum_wage:
+        average_foreign_wage = minimum_wage#preventing division by zero
+    D[r] += 0.00001 
+    
+    average_profits_other_reg = round( model.datacollector.model_vars['Regional_average_profits_cons'][int(model.schedule.time)][1-r])
+    #comp = model.datacollector.model_vars['Competitiveness_Regional'][int(model.schedule.time)]
+    #profits = min(0 , profits)
+    if average_profits_other_reg  <= 0: 
+        delta_profits = 0
+    else:
+        if profits <= 0:
+            profits = 0.0001
+        delta_profits = (profits - average_profits_other_reg) / profits
+ 
+  
+    demand_distance = (D[r] - D[1-r]) / D[r]
+    wage_distance = (average_foreign_wage   - my_wage) / (average_foreign_wage ) 
+
+    prob_migration =  1 - math.exp(w_1 * demand_distance  +  w_2* wage_distance +  w_3 * delta_profits)
+    #prob_migration = prob_migration * (1 - mc)
+    #print("F cons region ", r,  "my demand", D, " my wage " , my_wage, " avergae wage exp ", average_foreign_wage, " my profits ", profits, "prob migr", prob_migration)
+     #     "profits exp ", foreign_average_profits, " profitability", delta_profits, "prob migration ", prob_migration)
+	 #prob_migration =  1 - math.exp(w_1 * (D[r] - D[1-r]) / D[r] +  w_2*(average_foreign_wage - my_wage) / average_foreign_wage)
+    return prob_migration if prob_migration > 0 else 0
+
+
+
+'''
+def cap_firms_migration_probability(D, r, my_wage,profits, model, unemployment_subsidy, w_1 = 0.4, w_2 = 0.3, w_3 = 0.3, mc = 0.05):
+
+    average_foreign_wage = model.datacollector.model_vars['Average_Salary_Capital'][int(model.schedule.time)][1-r]
+    regional_unemployment_subsidy = model.datacollector.model_vars["Regional_unemployment_subsidy"][int(model.schedule.time)][r]
+    minimum_wage = model.datacollector.model_vars["Minimum_wage"][int(model.schedule.time) ]
+    if average_foreign_wage <= minimum_wage:
+        average_foreign_wage = minimum_wage#preventing division by zero
+    wage_distance = (average_foreign_wage - my_wage ) / (average_foreign_wage )
+    #comp = model.datacollector.model_vars['Competitiveness_Regional'][int(model.schedule.time)]
+    average_profits_other_reg = round( model.datacollector.model_vars['Regional_average_profits_cap'][int(model.schedule.time)][1-r])
+    #comp = model.datacollector.model_vars['Competitiveness_Regional'][int(model.schedule.time)]
+    #profits = min(0 , profits)
+    if average_profits_other_reg <= 0: 
+        delta_profits = 0
+    else:
+        if profits <= 0:
+            profits = 0.0001
+        delta_profits = (profits -  average_profits_other_reg) / profits
+        
+    #profits = min( 0, profits)
+    prob_migration = 1 - math.exp(w_1 * D + w_2* wage_distance + w_3 * delta_profits)
+    #prob_migration = prob_migration * (1 - mc)
+  
+    print(   "  my demand distance is ", \
+          D," my wage is ", my_wage, " average foreign wage is  ", average_foreign_wage, \
+              " this my wage distance is ", wage_distance, "final prob is ", prob_migration )
+
+    return prob_migration if prob_migration > 0 else 0
+'''  
+
+
+'''
+firm migration procedure
+'''
+def firm_migrate(mp, model, region, unique_id, employees_IDs, net_worth, wage):
+    firm = model.schedule._agents[unique_id]
+    ##-- if the probability is higher than random number between 0,1, start procedure to leave the region--##
+    if random.uniform(0,1) < mp:
+        ##--migration costs for each worker --## (need to add one for capital)
+        migration_cost = len(employees_IDs) * wage 
+        ##--- if migration is affordable --##
+        if migration_cost < 0.5 * net_worth:
+            ##--remove migration costs from ym net worth, fire employees and move out --##
+            net_worth -= migration_cost
+            for id in employees_IDs:
+                employee = model.schedule._agents[id]
+                employee.employer_ID = None
+            employees_IDs = []
+            if region == 0:
+                model.ids_region0.remove(unique_id)
+                bisect.insort(model.ids_region1, unique_id)
+            elif region == 1:
+                model.ids_region1.remove(unique_id)
+                bisect.insort(model.ids_region0, unique_id)
+            
+            model.grid.move_agent(firm, (0, 1-region))
+            
+    
+            return 1-region, employees_IDs, net_worth
+    
+    return region, employees_IDs, net_worth
